645fa5a46c303adb3298c0cfc2650c6d857ba4d5
dpp17 <durga123q.com>
2023-02-01 07:33:54 +0530

    Added: number of times the dice was played to win and psition

---
+++ day04snakeandladdergame/NoOfTimesDiceRolled.java
@@ -0,0 +1,14 @@
+// UC-6 $$ Report the number of times the dice was played to win the game and also the position after every die role
+package day04snakeandladdergame;
+
+public class NoOfTimesDiceRolled {
+
+	public static void main(String[] args) {
+		
+		EnsurePlayerGetExactHundred.main(args);
+		
+		System.out.println('\n'+"Number of times Dice Rolled :: "+ CheckIfLadderSnakeOrNoPlay.count);
+
+	}
+
+}
