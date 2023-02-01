2da3f6e32ed4076e22da9c70a2174f9b1353ffcc
dpp17 <durga123q.com>
2023-02-01 07:31:18 +0530

    Added: player gets to exact winning position 100

---
+++ day04snakeandladdergame/EnsurePlayerGetExactHundred.java
@@ -0,0 +1,15 @@
+// UC-5 $$ Ensure the player gets to exact winning position 100.
+package day04snakeandladdergame;
+
+
+public class EnsurePlayerGetExactHundred {
+	
+	
+//main method
+	public static void main(String[] args) {
+		PlayTillPlayerReachHundred.main(args);
+
+		}
+	}
+
+
